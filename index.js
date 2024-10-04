a3657131ad81afcc62c5103269ea47bfad51b79b
thanhDo220404 <chodenmot@gmail.com>
2024-10-04 21:14:38 +0700

    đổi port thành 2204

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2204;
 app
   .listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
