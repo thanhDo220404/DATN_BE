922cca50d95b3669cdee8eb08713b2136ecd5182
thanhDo220404 <chodenmot@gmail.com>
2024-10-08 15:19:59 +0700

    .

--- routes/users.js
+++ routes/users.js
@@ -103,7 +103,7 @@ router.post("/forgot", async (req, res) => {
         `<h1 style="color: red;">${newPass}</h1>`
       );
       await userController.resetPass(checkMail.userId, newPass);
-      res.status(200).json("Đã gửi mật khẩu mới về Email của bạn");
+      return res.status(200).json("Đã gửi mật khẩu mới về Email của bạn");
     } else {
       return res.status(404).json("Email này không tồn tại");
     }
